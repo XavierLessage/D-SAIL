c2a12c290ba24bf701693c51339a3069a5fe60c4
Maio175 <antoine.maiorca@umons.ac.be>
2021-09-07 16:02:33 +0200

    federated learning

---
+++ server/server.py
@@ -0,0 +1,35 @@
+import flwr as fl
+from flwr.server.strategy import FedAvg
+
+from typing import Callable, Dict, List, Optional, Tuple
+
+import numpy as np 
+
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(
+        self, 
+        rnd:int, 
+        results, 
+        failures, 
+    )-> Optional[fl.common.Weights]:
+        weights = super().aggregate_fit(rnd, results, failures)
+        if weights is not None:
+            #Save weights
+            print(f"Saving round {rnd} weights...")
+            np.savez(f"round-{rnd}-weights.npz", *weights)
+        return weights
+
+if __name__ == '__main__':
+
+    
+    strategy = SaveModelStrategy(
+        fraction_fit = 1.0,
+        min_fit_clients=3,
+        min_available_clients = 3,
+        min_eval_clients = 3
+    )
+    
+
+
+    #fl.server.start_server(config={"num_rounds":1})
+    fl.server.start_server(strategy=strategy, config={"num_rounds": 3})
