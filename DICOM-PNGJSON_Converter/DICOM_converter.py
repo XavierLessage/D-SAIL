722a54570b38a46298c1b98083d63fd50550dd08
DelinteNicolas <70629561+DelinteNicolas@users.noreply.github.com>
2021-09-07 09:18:16 +0200

    Update DICOM_converter.py
    Added getTagFromJson function

--- DICOM-PNGJSON_Converter/DICOM_converter.py
+++ DICOM-PNGJSON_Converter/DICOM_converter.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-
+Created on Wed Sep  1 15:44:11 2021
+@author: to fill
 Module with functions used to convert DICOM files (.dcm) to .png/.bmp and .json files and from .png/.bmp to DICOM. 
-
 The function 'compressToPng' calls executables from OpenJPEG (https://www.openjpeg.org/) available at 'https://github.com/uclouvain/openjpeg/releases/tag/v2.4.0'. 
-
 Required packages : 
     os
     random
@@ -14,7 +13,6 @@ Required packages :
     cv2         (opencv-python)
     pydicom
     numpy
-
 """
 
 import os
@@ -26,37 +24,33 @@ import cv2
 import pydicom
 import numpy as np
 
-def imgFromDICOM(dcm):
+def imgFromDICOM(ds):
     '''
     Extract array from dicom dataset 'dcm' with [0,256] pixel intensities.
-
     Parameters
     ----------
     dcm : FileDataset object of pydicom.dataset module
-
     Returns
     -------
     d : array
         Image array of the dicom dataset
-
     '''
 
-    data=dcm.pixel_array
+    data=ds.pixel_array
     
-    if 'WindowWidth' in dcm:
+    if 'WindowWidth' in ds:
     
         # Uses window levels written in dicom header    
-        data = pydicom.pixel_data_handlers.util.apply_voi_lut(dcm.pixel_array, dcm)
+        data = pydicom.pixel_data_handlers.util.apply_voi_lut(ds.pixel_array, ds)
         
-    d = np.round(cv2.normalize(data,  None, 0, 255, cv2.NORM_MINMAX)).astype('uint8')
+    img = np.round(cv2.normalize(data,  None, 0, 255, cv2.NORM_MINMAX)).astype('uint8')
     
-    return d
+    return img
     
 def decomposeDICOM(file_path,output_path,img_format='bmp',removeImgInJson=False):
     '''
     Divides dicom file into a .json file with the dicom metadata and a 
     .'img_format' file containing the image.
-
     Parameters
     ----------
     file_path : string
@@ -67,11 +61,9 @@ def decomposeDICOM(file_path,output_path,img_format='bmp',removeImgInJson=False)
         Image file format : bmp, png, ... The default is 'bmp'.
     removeImgInJson : True/False, optional
         Removes PixelData from dicom metadata. The default is False.
-
     Returns
     -------
     None.
-
     '''
     
     filename=file_path.rsplit("/")[-1]
@@ -80,16 +72,16 @@ def decomposeDICOM(file_path,output_path,img_format='bmp',removeImgInJson=False)
     
     # Open DICOM
     
-    dcm = pydicom.dcmread(file_path,force=True)
+    ds = pydicom.dcmread(file_path,force=True)
     
-    d = imgFromDICOM(dcm)
+    img = imgFromDICOM(ds)
     
     if removeImgInJson==True:
-        dcm.PixelData=None
+        ds.PixelData=None
     
-    metadata=dcm.to_json_dict()
+    metadata=ds.to_json_dict()
     
-    cv2.imwrite(output_path+filename+'.'+img_format, d)
+    cv2.imwrite(output_path+filename+'.'+img_format, img)
     with open(output_path+filename+'.json','w') as outfile:
         json.dump(metadata, outfile)
     
@@ -97,7 +89,6 @@ def dicomFromImgOrJson(file_path,output_folder,metadata_path=None,
                        randomizeName=False,verbose=False):
     '''
     Creates a dicom from a .json file or a .png/.bmp file
-
     Parameters
     ----------
     file_path : string
@@ -110,11 +101,9 @@ def dicomFromImgOrJson(file_path,output_folder,metadata_path=None,
         Creates a random patientID, optional
     verbose : True/False, optional
         Raises warnings. The default is False.
-
     Returns
     -------
     None.
-
     '''
     
     file_path_ex=file_path
@@ -124,7 +113,6 @@ def dicomFromImgOrJson(file_path,output_folder,metadata_path=None,
     file_path='.'.join(file_path)
     
     filename=file_path.rsplit("/")[-1]
-    #filename=filename.rsplit(".")[:-1]
     
     if metadata_path is not None and os.path.exists(metadata_path):
         ds = pydicom.dcmread(metadata_path,force=True)
@@ -180,7 +168,6 @@ def dicomFromImgOrJson(file_path,output_folder,metadata_path=None,
 def compressToPng(file_path,software_root,compress_ratio=1):
     '''
     Compresses an image to a .png with a specified 'compress_ratio'
-
     Parameters
     ----------
     file_path : string
@@ -189,11 +176,9 @@ def compressToPng(file_path,software_root,compress_ratio=1):
         Path to the folder containing the openjpeg .exe programs
     compress_ratio : int, optional
         Best if multiple of 8. The default is 1.
-
     Returns
     -------
     None.
-
     '''
     
     subprocess.call(software_root+'opj_compress'+
@@ -205,4 +190,26 @@ def compressToPng(file_path,software_root,compress_ratio=1):
               ' -o '+file_path.rsplit(".",1)[0]+'.png')
     os.system('rm '+file_path.rsplit(".",1)[0]+'.j2k')
     
-  
+def getTagFromJson(json_path,tag):
+    '''
+    Get tag value from .json fiel containing DICOM metadata
+
+    Parameters
+    ----------
+    json_path : string
+        /.../dicominfo.json
+    tag : tuple of two elements
+        DICOM tag, must be in hexagonal format. ex: (0x10,0x20)
+
+    Returns
+    -------
+    value : Value stored in tag
+
+    '''
+    
+    ds_json=json.load(open(json_path+'.json'))
+    ds = pydicom.dataset.Dataset.from_json(ds_json)
+    
+    value=ds[tag].value
+    
+    return value
