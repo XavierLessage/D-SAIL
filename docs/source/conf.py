814d637436e32e7659a2c646678d6dde9b1fb4fa
Jonathan Samelson <jonathan@samelson.info>
2021-09-08 21:05:09 +0200

    Added missing requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(1, os.path.abspath('../../federated_learning/client'))
+sys.path.insert(2, os.path.abspath('../../dicom_pseudonymizer/'))
 
-import dicom_pseudonymizer.anonymizer
+# autodoc_mock_imports = ["pytorch" , "opacus", "flwr", "torchvision", "fastai", "fastcore"]
 
 # -- Project information -----------------------------------------------------
 
--- main.py
+++
@@ -1,4 +0,0 @@
-from dicom_pseudonymizer.anonymizer import main
-
-if __name__ == "__main__":
-    main()
