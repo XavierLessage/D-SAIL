333898e7176bba5a8da8e151b715988ddc99a0dd
Jonathan Samelson <jonathan@samelson.info>
2021-09-08 18:43:15 +0200

    insert path for federated learning client

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(1, os.path.abspath('../../federated_learning/client'))
 
 import dicom_pseudonymizer.anonymizer
 
