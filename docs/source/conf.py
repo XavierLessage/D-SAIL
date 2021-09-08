75b093ad430487d68735f9b59a9be105fc800e4f
DelinteNicolas <nicolas.delinte@uclouvain.be>
2021-09-08 17:23:56 +0200

    changed folder name, added quick acces for federated learning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+import dicom_pseudonymizer.anonymizer
+
 # -- Project information -----------------------------------------------------
 
 project = 'D-SAIL'
